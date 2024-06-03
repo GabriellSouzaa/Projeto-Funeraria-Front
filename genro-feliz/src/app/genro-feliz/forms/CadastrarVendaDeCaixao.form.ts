e3b72f527b6482680a5b232bca2b0629ec3a92d0
Matheus De Oliveira Rodrigues Saleh <100796455+MatheusSaleh@users.noreply.github.com>
2024-06-03 18:52:29 -0300

    :sparkles: feat(minor): cadastrar venda de caixao

--- genro-feliz/src/app/genro-feliz/forms/CadastrarVendaDeCaixao.form.ts
+++ genro-feliz/src/app/genro-feliz/forms/CadastrarVendaDeCaixao.form.ts
@@ -1,5 +1,19 @@
-import { FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 export const VendaDeCaixaoForm = new FormGroup({
-  
+  seller: new FormGroup({
+    id: new FormControl('', Validators.required),
+    nome: new FormControl('', Validators.required),
+    cargo: new FormControl('', Validators.required),
+  }),
+  coffin: new FormGroup({
+    id: new FormControl('', Validators.required),
+    material: new FormControl('', Validators.required),
+    cor: new FormControl('', Validators.required),
+    comprimento: new FormControl('', Validators.required),
+    modelo: new FormControl('', Validators.required),
+    preco: new FormControl('', Validators.required)
+  }),
+  dataVenda: new FormControl('', Validators.required)
 })
+
--- genro-feliz/src/app/genro-feliz/venda-de-caixao/venda-de-caixao.component.ts
+++ genro-feliz/src/app/genro-feliz/venda-de-caixao/venda-de-caixao.component.ts
@@ -9,31 +9,119 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { TooltipModule } from 'primeng/tooltip';
+import { DialogModule } from 'primeng/dialog';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VendaDeCaixaoForm } from '../forms/CadastrarVendaDeCaixao.form';
+import { DropdownModule } from 'primeng/dropdown';
+import { VendedoresService } from '../shared/services/vendedores.service';
+import { CaixaoService } from '../shared/services/caixao.service';
+import { Vendedor } from '../shared/models/Vendedor.model';
+import { Caixao } from '../shared/models/Caixao.model';
 
 @Component({
   selector: 'app-venda-de-caixao',
   standalone: true,
-  imports: [TableModule, ProgressSpinnerModule, BreadcrumbComponent, NgIf, InputIconModule, InputTextModule, IconFieldModule, TooltipModule],
+  imports: [
+    TableModule,
+    ProgressSpinnerModule,
+    BreadcrumbComponent,
+    NgIf,
+    InputIconModule,
+    InputTextModule,
+    IconFieldModule,
+    TooltipModule,
+    DialogModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DropdownModule,
+  ],
   templateUrl: './venda-de-caixao.component.html',
-  styleUrl: './venda-de-caixao.component.css'
+  styleUrl: './venda-de-caixao.component.css',
 })
 export class VendaDeCaixaoComponent {
-
-  constructor(private vendaDeCaixaoService: VendaDeCaixaoService) { }
+  constructor(
+    private vendaDeCaixaoService: VendaDeCaixaoService,
+    private vendedoresService: VendedoresService,
+    private caixaoService: CaixaoService
+  ) {}
 
   public vendasDeCaixao: VendaDeCaixao[] = [];
 
   public carregandoListasDeCaixao: boolean = false;
 
+  public carregandoCadastroDeCaixao: boolean = false;
+
+  public formularioCadastrarCaixao: FormGroup = new FormGroup({});
+
+  public visibleDialogCadastroVendaCaixao: boolean = false;
+
+  public vendedores: Vendedor[] = [];
+
+  public caixoes: Caixao[] = [];
+
+  public vendedorSelecionado: Vendedor = new Vendedor();
+
+  public caixaoSelecionado: Caixao = new Caixao();
+
   ngOnInit() {
     this.listarVendasDeCaixao();
+    this.formularioCadastrarCaixao = VendaDeCaixaoForm;
   }
 
-  public listarVendasDeCaixao(): void{
+  public listarVendasDeCaixao(): void {
     this.carregandoListasDeCaixao = true;
-    this.vendaDeCaixaoService.listarVendasDeCaixao().subscribe(vendas => {
+    this.vendaDeCaixaoService.listarVendasDeCaixao().subscribe((vendas) => {
       this.vendasDeCaixao = vendas;
       this.carregandoListasDeCaixao = false;
     });
+  }
+
+  public listarVendedores(): void{
+    this.vendedoresService.listarVendedores().subscribe(vendedores => {
+      this.vendedores = vendedores;
+    })
+  }
+
+  public listarCaixoes(): void{
+    this.caixaoService.listarCaixoes().subscribe(caixoes => {
+      this.caixoes = caixoes;
+    })
+  }
+
+  public abrirDialogCadastroVendaCaixao(): void {
+    this.listarVendedores();
+    this.listarCaixoes();
+    this.visibleDialogCadastroVendaCaixao = true;
+  }
+
+  public fecharDialogCadastroVendaCaixao(): void {
+    this.visibleDialogCadastroVendaCaixao = false;
+    this.formularioCadastrarCaixao.reset();
+  }
+
+  public cadastrarVendaDeCaixao(): void {
+    this.carregandoCadastroDeCaixao = true;
+    this.vendaDeCaixaoService
+      .cadastrarVendaDeCaixao(this.formularioCadastrarCaixao.value)
+      .subscribe(() => {
+        this.listarVendasDeCaixao();
+        this.carregandoCadastroDeCaixao = false;
+        this.fecharDialogCadastroVendaCaixao();
+      });
+  }
+
+  public selecionaVendedor(){
+    this.formularioCadastrarCaixao.get('seller.id')?.setValue(this.vendedorSelecionado.id);
+    this.formularioCadastrarCaixao.get('seller.nome')?.setValue(this.vendedorSelecionado.nome);
+    this.formularioCadastrarCaixao.get('seller.cargo')?.setValue(this.vendedorSelecionado.cargo);
+  }
+
+  public selecionaCaixao(){
+    this.formularioCadastrarCaixao.get('coffin.id')?.setValue(this.caixaoSelecionado.id);
+    this.formularioCadastrarCaixao.get('coffin.material')?.setValue(this.caixaoSelecionado.material);
+    this.formularioCadastrarCaixao.get('coffin.cor')?.setValue(this.caixaoSelecionado.cor);
+    this.formularioCadastrarCaixao.get('coffin.comprimento')?.setValue(this.caixaoSelecionado.comprimento);
+    this.formularioCadastrarCaixao.get('coffin.modelo')?.setValue(this.caixaoSelecionado.modelo);
+    this.formularioCadastrarCaixao.get('coffin.preco')?.setValue(this.caixaoSelecionado.preco);
   }
 }
